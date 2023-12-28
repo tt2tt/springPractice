b110c9568e422addc2e04205876f1e6b5027f297
taro <groveleffect@gmail.com>
2023-12-28 14:29:47 +0900

    カスタムログイン・ログアウト機能作成（DB未使用）

---
+++ src/main/java/com/example/portfolio/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/")
+        ).authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
--- src/main/java/com/example/portfolio/free/UserController.java
+++ src/main/java/com/example/portfolio/free/UserController.java
@@ -9,4 +9,9 @@ public class UserController {
     public String userIndex(){
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
